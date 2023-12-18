b85fc5daf06d2ce9b4105cd446d4e4ea8378cb48
N.Karthik Akshaj <akshajnk@gmail.com>
2023-12-18 19:57:06 +0530

    Version 2: Added few things

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -1,12 +1,37 @@
-"use client"
+"use client";
 import { useGlobalState } from "@/app/context/globalContextProvider";
+import Image from "next/image";
 import React from "react";
 import styled from "styled-components";
 
 function Sidebar() {
-  const {theme} = useGlobalState();
-  return <SidebarStyled>Sidebar</SidebarStyled>;
+  const { theme } = useGlobalState();
+  return (
+    <SidebarStyled theme={theme}>
+      <div className="Profile">
+        <div className="Profile-overlay">
+          <div className="image">
+            <Image
+              width={80}
+              height={80}
+              src="/Avatar1.png"
+              alt="Profile Image"
+            />
+          </div>
+          <span>Yuji</span>
+          <span>Itadori</span>
+        </div>
+      </div>
+      <ul className="nav-items"></ul>
+    </SidebarStyled>
+  );
 }
 
-const SidebarStyled = styled.nav``;
+const SidebarStyled = styled.nav`
+  width: 20rem;
+  position: relative;
+  background-color: #022836;
+  border: 0.13rem solid #f48c06;
+  border-radius: 1rem;
+`;
 export default Sidebar;
--- app/Providers/ContextProvider.tsx
+++ app/Providers/ContextProvider.tsx
@@ -7,6 +7,21 @@ interface Props {
   children: React.ReactNode;
 }
 function ContextProvider({ children }: Props) {
+  const [isReady, setIsReady] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 250);
+  }, []);
+
+  if (!isReady) {
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <span className="loader"></span>
+      </div>
+    );
+  }
   return <GlobalProvider>{children}</GlobalProvider>;
 }
 
